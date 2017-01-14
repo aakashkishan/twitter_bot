8a80527c80e89de6bf44b77c5371624f85a0a15c
aakashkishan@outlook.com <aakash_kishan@outlook.com>
2017-01-14 18:24:48 +0530

    The required Twitter API keys

---
+++ twitter_api_keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'sDXkdM8mK3i55VqHfmZz53l5c',
+  consumer_secret:      'hL80DEBzb9nXH0XtioG8ZooRQQztBTRG8b3HMvJj4GAdIQco6F',
+  access_token:         '819834118972338176-mhwwAKaidDmvaZXZtIXEBc4uKCz30Gt',
+  access_token_secret:  'nC83yMdKIVGhgaokUY9BwbgOf7TMFnfaqthNfcpnsHXti',
+  timeout_ms:           60*1000,
+}
